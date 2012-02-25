d94214600d1bee3c1365e670b186e3a6c8979fde
Matt Milosavljevic <mr.mmilo@gmail.com>
2012-02-25 23:27:52 +1100

    can now create and list posts

--- main.rb
+++ main.rb
@@ -5,7 +5,31 @@ MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com',10019, :pool_
 MongoMapper.database = 'upward'
 MongoMapper.database.authenticate('rootuser','rootpassword')
 
+class Post
+  include MongoMapper::Document
+  key :text, String
+end
+
 get '/' do
+  @posts = Post.all
   erb :index
+end
+
+get '/posts' do
+  @posts = Post.all
+  erb :list
+end
+
+post '/posts/new' do
+  post = Post.new
+  post.text = (params[:text])
+
+  if post.save
+    status 201
+  else
+    status 401
+  end
+
+  redirect '/posts'
 end
 
